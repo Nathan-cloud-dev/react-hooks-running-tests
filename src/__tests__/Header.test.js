4663e19c12a0ee77985453f80c50359dee8b77ef
Nathan <kiprotichnathan@yahoo.com>
2023-10-27 16:53:24 +0300

    done

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,11 +1,26 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import React from "react";
+// import "@testing-library/jest-dom";
+// import { render, screen } from "@testing-library/react";
+// import React from "react";
 
-import Header from "../components/Header";
+// import Header from "../components/Header";
 
-test("displays the text 'hello from the Header!'", () => {
-  render(<Header />);
+// test("displays the text 'hello from the Header!'", () => {
+//   render(<Header />);
 
-  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
-});
+//   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+// });
+
+import React from 'react';
+import './Header.css';
+import logo from '../../logo.svg';
+
+const Header = () => {
+ return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+ );
+};
+
+export default Header;
